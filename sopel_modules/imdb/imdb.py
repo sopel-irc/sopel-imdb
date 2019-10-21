92f5774fc9cc86d2e6e461a8496acb7fdfe47bc5
Rusty Bower <rbower@riotgames.com>
2019-10-21 08:52:57 -0500

    imdb: fixing bug with imdb regex group

--- sopel_modules/imdb/imdb.py
+++ sopel_modules/imdb/imdb.py
@@ -74,13 +74,14 @@ def imdb(bot, trigger):
 def run_omdb_query(params, verify_ssl, api_key, add_url=True):
     uri = "http://www.omdbapi.com/"
     if 'i' in params:
-        data = requests.get(uri, params={'i': params['i'], 'apikey': api_key}, timeout=30, verify=verify_ssl)
+        data = requests.get(uri, params={'i': 'tt{}'.format(params['i']), 'apikey': api_key}, timeout=30, verify=verify_ssl)
     elif 'y' in params:
         data = requests.get(uri, params={'t': params['t'], 'y': params['y'], 'apikey': api_key}, timeout=30, verify=verify_ssl)
     else:
         data = requests.get(uri, params={'t': params['t'], 'apikey': api_key}, timeout=30, verify=verify_ssl)
 
     data = data.json()
+    print(data)
     if data['Response'] == 'False':
         if 'Error' in data:
             message = '[Error] %s' % data['Error']
