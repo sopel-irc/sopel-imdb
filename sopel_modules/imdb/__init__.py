25636e02e6985eee7d4a7df08fa5c95d4ec025c2
Rusty Bower <rbower@riotgames.com>
2019-11-09 09:53:37 +0000

    imdb: lgtm fixes

--- sopel_modules/imdb/__init__.py
+++ sopel_modules/imdb/__init__.py
@@ -4,7 +4,7 @@ A working re-implementation of the weather module for Sopel
 """
 from __future__ import unicode_literals, absolute_import, division, print_function
 
-from .imdb import *
+from .imdb import imdb, imdb_url
 
 __author__ = 'Rusty Bower'
 __email__ = 'rusty@rustybower.com'
