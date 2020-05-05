13ae69b37e3747646b1b7a42136774c5c42df6f1
Rusty Bower <rusty@rustybower.com>
2020-05-05 10:36:58 -0500

    init: fixing description from failed copy paste

--- sopel_modules/imdb/__init__.py
+++ sopel_modules/imdb/__init__.py
@@ -1,6 +1,6 @@
 # coding=utf8
 """
-A working re-implementation of the weather module for Sopel
+A working re-implementation of the imdb module for Sopel
 """
 from __future__ import unicode_literals, absolute_import, division, print_function
 
