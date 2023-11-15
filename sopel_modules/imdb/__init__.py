29dfc53a7e1e11d47ce943f96dfd8b1ef8112cff
xnaas <me@xnaas.info>
2023-11-15 13:23:43 -0600

    __init__.py: remove unused imports

--- sopel_modules/imdb/__init__.py
+++ sopel_modules/imdb/__init__.py
@@ -4,7 +4,6 @@ A working re-implementation of the imdb module for Sopel
 """
 from __future__ import unicode_literals, absolute_import, division, print_function
 
-from .imdb import configure, imdb, imdb_url, setup
 
 __author__ = 'Rusty Bower'
 __email__ = 'rusty@rustybower.com'
